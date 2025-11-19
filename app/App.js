80e9c5fabba7082bbffc5f6d6a7ca82ee3a7dd1b
AlandisAyupov <steamuservni@gmail.com>
2025-11-18 21:24:35 -0500

    feat: add auth context

--- app/App.js
+++ app/App.js
@@ -3,21 +3,23 @@ import { createStackNavigator } from '@react-navigation/stack';
 import WelcomeScreen from './src/screens/welcome';
 import LoginScreen from './src/screens/login';
 import AccountCreationScreen from './src/screens/accountCreation';
+import { AuthProvider } from './src/contexts/AuthContext';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Welcome"
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen name="Welcome" component={WelcomeScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Create-Account" component={AccountCreationScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-
+    <AuthProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Welcome"
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen name="Welcome" component={WelcomeScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Create-Account" component={AccountCreationScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AuthProvider>
   );
 }
---
+++ app/src/contexts/AuthContext.js
@@ -0,0 +1,33 @@
+import React, { createContext, useState, useEffect, useContext } from 'react';
+import { auth } from '../../config/firebase';
+import { onAuthStateChanged } from 'firebase/auth';
+
+const AuthContext = createContext({});
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unmount = onAuthStateChanged(auth, (firebaseUser) => {
+      setUser(firebaseUser);
+      setLoading(false);
+    });
+
+    return unmount;
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+};
